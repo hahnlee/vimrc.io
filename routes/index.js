47149fc77f3cca6e572725b143ece3c04e234974
sn0wle0pard <hanlee@unist.ac.kr>
2016-11-03 20:50:09 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'vimrc.io' });
+});
+
+module.exports = router;
