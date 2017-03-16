013267a90b741d9e6f2c5a0b8ad63f0803fdfbaa
sn0wle0pard <hanlee@unist.ac.kr>
2017-03-16 13:35:47 +0900

    fix defendant

--- config.sample.js
+++ config.sample.js
@@ -1,23 +1,8 @@
 var config = {};
 
-/* server port */
-config.web = {};
-config.web.port = 3000;
-config.web.devPort = 4000;
-
-/* session */
-config.session = {};
-config.session.secret= '';
-config.session.name = '';
-
-/* Mongodb */
-config.mongodb= {};
-/* id : pw @ host:port / db_name */
-config.mongodb.connectUrl = '';
-
-/* crypto */
-config.crypto = {};
-config.crypto.password = '';
+/* paypal */
+config.paypal = {};
+config.paypal.encryped = '';
 
 module.exports = config;
 
