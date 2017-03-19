5f76345a5ec5c363f5c268a7b0bf6fa664067b9a
sn0wle0pard <hanlee@unist.ac.kr>
2017-03-19 18:25:44 +0900

    porting build script

--- config/paths.js
+++ config/paths.js
@@ -55,5 +55,5 @@ module.exports = {
   appNodeModules: resolveApp('node_modules'),
   nodePaths: nodePaths,
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  savedPath: getServedPath(resolveApp('package.json'))
+  servedPath: getServedPath(resolveApp('package.json'))
 };
