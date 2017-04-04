3a684affb5dc3df837b4e417946d377acbbb5712
sn0wle0pard <hanlee@unist.ac.kr>
2017-04-04 22:12:54 +0900

    change to stateless component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './Header.scss';
 
-class Header extends React.Component {
-  render() {
-    const logoImage = require('assets/logo.png');
+const Header = () => {
+  const logoImage = require('assets/logo.png');
     
-    return (
-      <header className="header">
-        <div className="nav-logo">
-          <img src={logoImage} alt="logo"/>
-        </div>
-        <div className="nav">
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="header">
+      <div className="nav-logo">
+        <img src={logoImage} alt="logo"/>
+      </div>
+      <div className="nav">
+      </div>
+    </header>
+  );
 }
 
 export default Header;
